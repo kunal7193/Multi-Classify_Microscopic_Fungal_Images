93b1fb9d7a9f1dbb3c55f5f9fc1bb8284fd4d33d
vijen.mehta <88127200+Vijen712@users.noreply.github.com>
2023-12-14 21:39:25 -0800

    Add files via upload

--- Code/train_evaluate.py
+++ Code/train_evaluate.py
@@ -125,7 +125,7 @@ def train_and_evaluation_densenet(model, train_generator, valid_generator, test_
 
     return history_densenet, predictions, true_classes, class_labels, fpr, tpr, roc_auc, cm
 
-def train_and_evaluation_resnet(model, train_generator, valid_generator, test_generator, epochs=40): #change back to 40 after test
+def train_and_evaluation_resnet(model, train_generator, valid_generator, test_generator, epochs=40): #change back to 40 after testing
     initial_learning_rate = 0.0005
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate, decay_steps=1000, decay_rate=0.9)
@@ -176,7 +176,7 @@ def train_and_evaluation_resnet(model, train_generator, valid_generator, test_ge
     return history, predictions, true_classes, class_labels, fpr, tpr, roc_auc, cm
 
 
-def train_and_evaluate_miniVGG_Var1(model, train_generator, valid_generator, test_generator, epochs=100):  # change epochs to 100 after test
+def train_and_evaluate_miniVGG_Var1(model, train_generator, valid_generator, test_generator, epochs=100):  # change epochs to 100 after testing
     # Define the initial learning rate
     initial_learning_rate = 0.00005
 
@@ -227,5 +227,61 @@ def train_and_evaluate_miniVGG_Var1(model, train_generator, valid_generator, tes
     cm = confusion_matrix(true_classes, predicted_classes)
 
     return history, predictions, true_classes, class_labels, fpr, tpr, roc_auc, cm
+
+
+def train_and_evaluate_densenet_var(model, train_generator, valid_generator, test_generator, epochs=100): # change epochs to 100 after testing
+    initial_learning_rate = 0.0005
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate, decay_steps=1000, decay_rate=0.9)
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
+                  metrics=['accuracy'])
+
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', patience=10, restore_best_weights=True)
+
+    start_time = time.time()
+    history = model.fit(
+        train_generator,
+        steps_per_epoch=train_generator.samples // train_generator.batch_size,
+        epochs=epochs,
+        validation_data=valid_generator,
+        validation_steps=valid_generator.samples // valid_generator.batch_size,
+        callbacks=[early_stopping])
+    end_time = time.time()
+    training_time = end_time - start_time
+    print(f"Total training time: {training_time:.2f} seconds")
+
+    # Evaluate model on test set
+    test_loss, test_accuracy = model.evaluate(
+        test_generator, steps=test_generator.samples // test_generator.batch_size)
+    print(f"Test Accuracy: {test_accuracy}")
+
+    # Generate predictions for evaluation metrics
+    predictions = model.predict(test_generator)
+    predicted_classes = np.argmax(predictions, axis=1)
+    true_classes = test_generator.classes
+    class_labels = list(test_generator.class_indices.keys())
+
+    # Classification report
+    report = classification_report(true_classes, predicted_classes, target_names=class_labels)
+    print(report)
+
+    # ROC Curve and AUC
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    n_classes = test_generator.num_classes
+    y_test = tf.keras.utils.to_categorical(true_classes, num_classes=n_classes)
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], predictions[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Confusion Matrix
+    cm = confusion_matrix(true_classes, predicted_classes)
+
+    return history, predictions, true_classes, class_labels, fpr, tpr, roc_auc, cm
+
 
 
